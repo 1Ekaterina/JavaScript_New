fe99fff9e8d230cc16bc606d3ecba20005ece6c9
Ekaterina <kh.katerina@bk.ru>
2021-06-15 15:09:26 +0300

    all

--- js/ProducComp.js
+++ js/ProducComp.js
@@ -8,8 +8,8 @@ Vue.component('products', {
         }
     },
     methods: {
-        filter(){
-            let regexp = new RegExp(this.userSearch, 'i');
+        filter(value){
+            let regexp = new RegExp(value, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
@@ -24,12 +24,23 @@ Vue.component('products', {
     },
     template: `
         <div class="products">
-            <product v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+            <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
         </div>
     `
 });
 Vue.component('product', {
     props: ['product', 'img'],
+    data() {
+      return {
+          /**
+           * Создали ссылку на API нашей корзины. Т.к. все компоненты у нас регистрируются в корневом экземпляре Vue,
+           * то мы легко можем получить доступ к ним используя свойство $root.
+           * $parent можно использовать для доступа к родительскому экземпляру из дочернего.
+           */
+          cartAPI: this.$root.$refs.cart,
+      };
+    },
+
     template: `
     <div class="product-item">
                 <img :src="img" alt="Some img">
