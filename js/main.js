77b759bc15e8423e022d1bacbd99e59a1bf5ee8c
Ekaterina <kh.katerina@bk.ru>
2021-06-15 14:26:20 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+    },
+    mounted() {
+        console.log(this);
+    }
+});
+
